ac41d7767f968383dc972cb7cc6967e7dcfe0664
mVishwajeet <mannepallivishwajeet@gmail.com>
2022-10-10 16:49:13 -0600

    Added home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: redirect('/home')
+  get 'home' , to: 'pages#home', as: 'home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
